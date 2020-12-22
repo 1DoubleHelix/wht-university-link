c2169b5e08f599205220bc0a2f8a6bcd55703b6c
1DoubleHelix <66552330+1DoubleHelix@users.noreply.github.com>
2020-12-22 23:33:26 +0800

    Add Guangzhou University

---
+++ 12007.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "广州大学",
+    id: "12007",
+    link: [ {
+        name: "广大主页",
+        url: "http://www.gzhu.edu.cn/"
+    }, {
+        name: "信息门户",
+        url: "https://cas.gzhu.edu.cn/cas_server/login"
+    }, {
+        name: "图书馆",
+        url: "http://lib.gzhu.edu.cn/"
+    }, {
+        name: "广大邮箱",
+        url: "http://mail.e.gzhu.edu.cn/"
+    }, {
+        name: "SSLVPN",
+        url: "https://vpn.gzhu.edu.cn/"
+    }, {
+        name: "网络课堂",
+        url: "http://gzhdx.fanya.chaoxing.com/"
+    }, ]
+};
